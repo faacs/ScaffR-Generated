6a6a570066c64db534a5f161186bc720fd1125a3
kuis <williamjin8683@gmail.com>
2013-03-11 14:50:37 +0200

    03/11/2013__01

--- DemoApplication/DemoApplication/Controllers/Account/AccountController.Emails.cs
+++ DemoApplication/DemoApplication/Controllers/Account/AccountController.Emails.cs
@@ -33,6 +33,6 @@ namespace DemoApplication.Controllers.Account
             }
 
             return RedirectToAction("Emails");
-        }    
+        }
     }
 }
--- DemoApplication/DemoApplication/Controllers/Account/AccountController.Photo.cs
+++ DemoApplication/DemoApplication/Controllers/Account/AccountController.Photo.cs
@@ -32,11 +32,23 @@ namespace DemoApplication.Controllers.Account
             var user = UserProfile.Current;
             
             user.PhotoId = photo[0].Id;
-            _userService.SaveOrUpdate(user);
+
+            /*_userService.SaveOrUpdate(user);
 
             TempData["Success"] = "Profile photo was successfully updated";
+            return RedirectToAction("Photo"); */
 
-            return RedirectToAction("Photo");
+            /* Update by William 03/11/2013 */
+
+            var result = user.Save();
+
+            if (ModelState.Process(result))
+            {
+                TempData["Success"] = "Profile photo was successfully updated";
+                return RedirectToAction("Photo");
+            }
+            return View();
+            /* **************************** */
         }
     }
 }
